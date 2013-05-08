3a24835990576ccd5ab8a10161c4598149ce18bb
Mariano Ravinale <mravinale@gmail.com>
2013-05-08 15:03:07 -0300

    removed unit testing dependencies

--- Prometeo.Tests/Controllers/ValuesControllerTest.cs
+++ Prometeo.Tests/Controllers/ValuesControllerTest.cs
@@ -29,53 +29,53 @@ namespace Prometeo.Tests.Controllers
         //    Assert.AreEqual("value2", result.ElementAt(1));
         //}
 
-        [TestMethod]
-        public void GetById()
-        {
-            // Disponer
-            ValuesController controller = new ValuesController();
+        //[TestMethod]
+        //public void GetById()
+        //{
+        //    // Disponer
+        //    ValuesController controller = new ValuesController();
 
-            // Actuar
-            string result = controller.Get(5);
+        //    // Actuar
+        //    string result = controller.Get(5);
 
-            // Declarar
-            Assert.AreEqual("value", result);
-        }
+        //    // Declarar
+        //    Assert.AreEqual("value", result);
+        //}
 
-        [TestMethod]
-        public void Post()
-        {
-            // Disponer
-            ValuesController controller = new ValuesController();
+        //[TestMethod]
+        //public void Post()
+        //{
+        //    // Disponer
+        //    ValuesController controller = new ValuesController();
 
-            // Actuar
-            controller.Post("value");
+        //    // Actuar
+        //    controller.Post("value");
 
-            // Declarar
-        }
+        //    // Declarar
+        //}
 
-        [TestMethod]
-        public void Put()
-        {
-            // Disponer
-            ValuesController controller = new ValuesController();
+        //[TestMethod]
+        //public void Put()
+        //{
+        //    // Disponer
+        //    ValuesController controller = new ValuesController();
 
-            // Actuar
-            controller.Put(5, "value");
+        //    // Actuar
+        //    controller.Put(5, "value");
 
-            // Declarar
-        }
+        //    // Declarar
+        //}
 
-        [TestMethod]
-        public void Delete()
-        {
-            // Disponer
-            ValuesController controller = new ValuesController();
+        //[TestMethod]
+        //public void Delete()
+        //{
+        //    // Disponer
+        //    ValuesController controller = new ValuesController();
 
-            // Actuar
-            controller.Delete(5);
+        //    // Actuar
+        //    controller.Delete(5);
 
-            // Declarar
-        }
+        //    // Declarar
+        //}
     }
 }
